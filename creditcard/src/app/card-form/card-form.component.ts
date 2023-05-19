be450a162fa70e58ca1881ce50880e136be8bf3b
Hannah Fernandes-Martin <hannah.fernandes-martin@liquidware.com>
2023-05-19 10:53:20 -0400

    added form validators

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -12,11 +12,28 @@ export class CardFormComponent {
     name: new FormControl('', [
       Validators.required, 
       Validators.minLength(3),
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
     ])
   });
 
   constructor() {
     console.log(this.cardForm.get('name'))
   }
-
+  
+  onSubmit() {
+    console.log('form submitted')
+  }
 }
